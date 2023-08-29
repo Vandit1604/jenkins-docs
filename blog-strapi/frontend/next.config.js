683df211beb8cb0f764ff6bfb2c07f5cc2ddfb63
Vandit Singh <vanditsinghkv@gmail.com>
2023-08-29 23:50:30 +0530

    add code nextjs + strapi blog

---
+++ blog-strapi/frontend/next.config.js
@@ -0,0 +1,25 @@
+// ./frontend/next.config.js
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '127.0.0.1',
+          port: '1337',
+          pathname: '/uploads/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.pexels.com',
+        }
+      ],
+    },
+  }
+  
+  module.exports = nextConfig
