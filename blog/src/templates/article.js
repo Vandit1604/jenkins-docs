13edbdaf7d03a0b52253d4ce9354159c657aa922
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-06 17:51:40 +0530

    use allAsciidoc to query data into page

--- blog/src/templates/article.js
+++ blog/src/templates/article.js
@@ -7,18 +7,18 @@ class Article extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>{this.props.data.asciidoc.document.title} </h1>
-        {this.props.data.asciidoc.author && (
+        <h1>{this.props.data.allAsciidoc.document} </h1>
+        {this.props.data.allAsciidoc.author && (
           <table>
             <tbody>
               <tr>
-                <td>{this.props.data.asciidoc.author.fullName}</td>
+                <th>{this.props.data.allAsciidoc.pageAttributes.author}</th>
               </tr>
             </tbody>
           </table>
         )}
         <div
-          dangerouslySetInnerHTML={{ __html: this.props.data.asciidoc.html }}
+          dangerouslySetInnerHTML={{ __html: this.props.data.allAsciidoc.html }}
         />
       </Layout>
     )
@@ -28,15 +28,19 @@ class Article extends React.Component {
 export default Article
 
 export const pageQuery = graphql`
-  query($id: String!) {
-    asciidoc(id: { eq: $id }) {
-      html
-      document {
-        title
-        main
-      }
-      author {
-        fullName
+  query{
+    allAsciidoc {
+      edges {
+        node {
+          html
+          document {
+            title
+          }
+           pageAttributes{
+            author
+            tags
+          }
+        }
       }
     }
   }
