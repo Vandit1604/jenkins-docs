7daa794fcd2a1fea41207dd47a45e761292e1da8
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-08 04:25:18 +0530

    add some styling using .css.modules

--- blog/src/layouts/index.js
+++ blog/src/layouts/index.js
@@ -12,21 +12,25 @@ class IndexPageLayout extends React.Component {
           margin: `0 auto`,
           marginTop: rhythm(1.5),
           marginBottom: rhythm(1.5),
-          maxWidth: 900,
+          maxWidth: 1500,
           paddingLeft: rhythm(3 / 4),
           paddingRight: rhythm(3 / 4),
-
         }}
       >
         <Link style={{ textDecoration: `none` }} to="/">
-          <img src="..././docs/images/modules/ROOT/assets/images/logos/jenkins/jenkins.png" alt="Jenkins Logo" width="200px" height="200px"></img>
+          <img
+            src="..././docs/images/modules/ROOT/assets/images/logos/jenkins/jenkins.png"
+            alt="Jenkins Logo"
+            width="200px"
+            height="200px"
+          ></img>
           <h3 style={{ color: `black`, marginBottom: rhythm(1.5) }}>
             Jenkins Blog
           </h3>
         </Link>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -2,8 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import IndexPageLayout from "../layouts"
-import IndexPageLayout from "../layouts"
-import { bloglisting } from "../css/blogpost.module.css"
+import { bloglisting, blogpost } from "../css/blogpost.module.css"
 
 class IndexPage extends React.Component {
   render() {
@@ -11,11 +10,27 @@ class IndexPage extends React.Component {
       <IndexPageLayout>
         <ul className={bloglisting}>
           {this.props.data.allAsciidoc.edges.map(({ node }) => (
-            <li key={node.id} className={bloglisting}>
+            <li key={node.id} className={blogpost}>
               <Link to={node.fields.slug}>
-                <img src={node.opengraph} alt="." width="200px" height="200px"></img>
-                {node.document.title}</Link>
-              <p>{node.pageAttributes.author}</p>
+                <img
+                  src={node.opengraph}
+                  alt="."
+                  width="200px"
+                  height="200px"
+                  display="block"
+                ></img>
+                {node.document.title}
+              </Link>
+              <br></br>
+              <img
+                src="https://www.jenkins.io/images/avatars/markewaite.jpg"
+                alt="Mark Waite"
+                style={{
+                  width: "20px",
+                  height: "20px",
+                  "border-radius": "50%",
+                }}
+              ></img><p>{node.pageAttributes.author}</p>
             </li>
           ))}
         </ul>
@@ -31,24 +46,14 @@ export const pageQuery = graphql`
     allAsciidoc {
       edges {
         node {
+          html
           fields {
             slug
-          }
-          html
-          revision {
-            date
-            number
-            remark
           }
           document {
             title
           }
           pageAttributes {
-            author
-            tags
-            opengraph
-          }
-          pageAttributes{
             author
             tags
             opengraph
