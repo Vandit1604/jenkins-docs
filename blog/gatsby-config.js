0ddd283ec17a6e4d897505c7676329fcfedcbe5e
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-09 22:12:14 +0530

    fix most of the opengraph images

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -14,14 +14,6 @@ module.exports = {
       options: {
         name: `pages`,
         path: `${__dirname}/content`,
-        imagesdir: `${__dirname}/content/post-images/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `post-images`,
-        path: `${__dirname}/content/post-images`,
       },
     },
     {
@@ -41,5 +33,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
     },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 };
