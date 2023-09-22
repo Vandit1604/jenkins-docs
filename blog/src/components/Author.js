2a4f2108c62890560ce16583781d0ca52b6d50cc
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-23 03:48:09 +0530

    fix in authors page

--- blog/src/components/Author.js
+++ blog/src/components/Author.js
@@ -4,34 +4,32 @@ import { useStaticQuery, graphql } from "gatsby"
 const Author = () => {
   const data = useStaticQuery(graphql` 
     query{
-      allAsciidoc(filter: {document: {title: {eq: "Author"}}}) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            document {
-              title
-              subtitle
-              main
-            }
-            pageAttributes {
-              tags
-              author
-              author_name
-              github
-              blog
-              linkedin
-              description
-              opengraph
-              irc
-              twitter
-              medium
-            }
-          }
+  allAsciidoc(filter: {document: {title: {eq: "Author"}}}) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        document {
+          title
+          main
+        }
+        pageAttributes {
+          tags
+          author_name
+          github
+          blog
+          linkedin
+          description
+          opengraph
+          irc
+          twitter
+          medium
         }
       }
     }
+  }
+}
   `)
   return (
     data.allAsciidoc.edges.map(({ node }) => {
@@ -43,7 +41,8 @@ const Author = () => {
               <img src="https://imgs.search.brave.com/7L4GDDrWn6V2T2S8W1BmMXcXeLj0KW5FsVdZkqpOz-c/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L3Bv/cnRhbHdvcmxkc2dh/bWUvaW1hZ2VzL2Ev/YTcvUmVkX0Jsb2Nr/LnBuZy9yZXZpc2lv/bi9sYXRlc3Qvc2Nh/bGUtdG8td2lkdGgt/ZG93bi8xMjg_Y2I9/MjAxOTA2MTExMTAz/NTQ.jpeg" style={{ borderRadius: "50%" }} width={128} height={128} />
               <h4>{node.pageAttributes.author_name}</h4>
             </div>
-            <p>{node.pageAttributes.description}</p>
+            <div dangerouslySetInnerHTML={{ __html: node.pageAttributes.description }}
+            />
           </div>
         </>
       )
