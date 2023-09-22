36cd217fbfac1ea637cbfc35ff00956a5f990830
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-23 04:04:46 +0530

    try 1: adding navbar and footer

--- blog/src/layouts/index.js
+++ blog/src/layouts/index.js
@@ -1,13 +1,15 @@
 import React from "react"
-
+import Navbar from "../../node_modules/@jenkinsci/jenkins-io-components/build/jio-navbar"
+import Footer from "../../node_modules/@jenkinsci/jenkins-io-components/build/jio-footer"
 import typography from "../utils/typography"
 const { rhythm } = typography
 
 class IndexPageLayout extends React.Component {
   render() {
     return (
+      <>
       <div
-        style={{
+          style={{
           margin: `0 auto`,
           marginTop: rhythm(1.5),
           marginBottom: rhythm(1.5),
@@ -15,9 +17,12 @@ class IndexPageLayout extends React.Component {
           paddingLeft: rhythm(3 / 4),
           paddingRight: rhythm(3 / 4),
         }}
-      >
+        >
+        <Navbar/>
         {this.props.children}
+        <Footer/>
       </div>
+      </>
     );
   }
 }
