12f526e872a5c71f1552da491758ca3ae9a2dafc
Vandit Singh <vanditsinghkv@gmail.com>
2023-09-23 04:37:26 +0530

    fix multiple references render problem; still not complete object is getting rendered

--- blog/src/pages/changelog-lts.js
+++ blog/src/pages/changelog-lts.js
@@ -1,16 +1,26 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import IndexPageLayout from "../layouts"
-import jenkinsLogo from "../../../docs/images/modules/ROOT/assets/images/logos/jenkins/jenkins.png"
-import { iconlegend, image, security, bug, rfe, feedback, sunny, cloudy, storm } from "../css/changelog.module.css";
-import typography from "../utils/typography"
-const { rhythm } = typography
+import React from "react";
+import { Link, graphql } from "gatsby";
+import IndexPageLayout from "../layouts";
+import jenkinsLogo from "../../../docs/images/modules/ROOT/assets/images/logos/jenkins/jenkins.png";
+import {
+  iconlegend,
+  image,
+  security,
+  bug,
+  rfe,
+  feedback,
+  sunny,
+  cloudy,
+  storm,
+} from "../css/changelog.module.css";
+import typography from "../utils/typography";
+const { rhythm } = typography;
 
 class IndexPage extends React.Component {
   render() {
-    console.log(this.props.data.allLtsYaml.edges)
+    console.log(this.props.data.allLtsYaml.edges);
     return (
-      < IndexPageLayout >
+      <IndexPageLayout>
         <Link style={{ textDecoration: `none` }} to="/">
           <h3
             style={{
@@ -40,12 +50,12 @@ class IndexPage extends React.Component {
             Legend:
             <ul className={image}>
               <li className={security}>security fix</li>
-              <li className={bug} >major bug fix</li>
+              <li className={bug}>major bug fix</li>
               <li className={bug}>bug fix</li>
               <li className={rfe}>major enhancement</li>
               <li className={rfe}>enhancement</li>
             </ul>
-          </div >
+          </div>
           <div style={{ margin: "10px 0", width: "100%" }}>
             <div className={iconlegend}>
               Community feedback:
@@ -55,85 +65,134 @@ class IndexPage extends React.Component {
                 <li className={storm}>required rollback</li>
               </ul>
             </div>
-          </div >
+          </div>
         </div>
-        <div style={{ margin: "10px", padding: "10px", backgroundColor: "#FFFFCE" }}>
-          See the <Link to="/upgrade-guide">LTS upgrade guide</Link> advice on upgrading Jenkins.
+        <div
+          style={{
+            margin: "10px",
+            padding: "10px",
+            backgroundColor: "#FFFFCE",
+          }}
+        >
+          See the <Link to="/upgrade-guide">LTS upgrade guide</Link> advice on
+          upgrading Jenkins.
         </div>
-        {this.props.data.allLtsYaml.edges.map(({ node }) =>
+        {this.props.data.allLtsYaml.edges.map(({ node }) => (
           <>
-            <h3>What's new in {node.version}({node.date}) </h3>
+            <h3>
+              What's new in {node.version}({node.date}){" "}
+            </h3>
             <p>Community reported issues : </p>
             <h4>Changes since {node.lts_baseline ?? ""}:</h4>
             <ul>
               {node.changes?.map((change) => {
                 return (
                   <li>
-                    <span dangerouslySetInnerHTML={{ __html: change.message }} />
-                    <span><a href={"https://issues.jenkins.io/browse/JENKINS-" + change.issue}>(issue {change.issue})</a></span>
+                    <span
+                      dangerouslySetInnerHTML={{ __html: change.message }}
+                    />
+                    <span>
+                      <a
+                        href={
+                          "https://issues.jenkins.io/browse/JENKINS-" +
+                          change.issue
+                        }
+                      >
+                        (issue {change.issue})
+                      </a>
+                    </span>
                   </li>
                 );
               })}
             </ul>
             <h4>Notable changes since {node.lts_predecessor ?? ""}:</h4>
             <ul>
-              {node.lts_changes?.map((changes) => {
+              {node.lts_changes?.map((references) => {
                 return (
                   <li>
-                    <span dangerouslySetInnerHTML={{ __html: changes.message }} />
-                    <span>
-                      {node.lts_changes?.map((references) => {
+                    <span
+                      dangerouslySetInnerHTML={{ __html: references.message }}
+                    />
+                    {(() => {
+                      if (references.issue != null) {
                         return (
-                          <span><a href={"https://github.com/jenkinsci/jenkins/pull/" + references.pull}> pull {references.pull},</a></span>
+                          <span>
+                            <a
+                              href={
+                                "https://issues.jenkins.io/browse/JENKINS-" +
+                                references.issue
+                              }
+                            >
+                              {" "}
+                              issue {references.issue},
+                            </a>
+                          </span>
                         );
-                      })}
-                    </span>
+                      }
+                    })()}
+                    {(() => {
+                      if (references.pull != null) {
+                        return (
+                          <span>
+                            <a
+                              href={
+                                "https://github.com/jenkinsci/jenkins/pull/" +
+                                references.pull
+                              }
+                            >
+                              {" "}
+                              pull {references.pull},
+                            </a>
+                          </span>
+                        );
+                      }
+                    })()}
                   </li>
                 );
               })}
-            </ul >
+            </ul>
           </>
-        )
-        }
-      </IndexPageLayout >
+        ))}
+      </IndexPageLayout>
     );
   }
 }
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
-query{
-  allLtsYaml(sort: {date: DESC}) {
-    edges {
-      node {
-        version
-        date(formatString: "DD-MM-YYYY")
-        lts_predecessor
-        lts_baseline
-        lts_changes {
-          type
-          category
-          pull
-          issue
-          message
-          pr_title
-          references {
-            issue
+  query {
+    allLtsYaml(sort: { date: DESC }) {
+      edges {
+        node {
+          version
+          date(formatString: "DD-MM-YYYY")
+          lts_predecessor
+          lts_baseline
+          lts_changes {
+            type
+            category
             pull
-            url
-            title
+            issue
+            message
+            pr_title
+            references {
+              issue
+              pull
+              url
+              title
+            }
           }
-        }
-        changes {
-          type
-          category
-          issue
-          message
-          pull
-          pr_title
+          changes {
+            type
+            category
+            issue
+            message
+            pull
+            pr_title
+          }
         }
       }
     }
   }
-}`
+`;
