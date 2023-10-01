eab5d1b81ccd2600d85970541c5d99e28724e679
Vandit Singh <vanditsinghkv@gmail.com>
2023-10-02 01:39:41 +0530

    add favicon on gatsby site

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -58,5 +58,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Jenkins",
+        short_name: "Jenkins",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: `src/images/jenkins.svg`, // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+
+
   ],
 };
--- blog/src/layouts/index.js
+++ blog/src/layouts/index.js
@@ -1,6 +1,4 @@
 import React from "react"
-import Navbar from "../../node_modules/@jenkinsci/jenkins-io-components/build/jio-navbar"
-import Footer from "../../node_modules/@jenkinsci/jenkins-io-components/build/jio-footer"
 import typography from "../utils/typography"
 const { rhythm } = typography
 
@@ -8,20 +6,18 @@ class IndexPageLayout extends React.Component {
   render() {
     return (
       <>
-      <div
+        <div
           style={{
-          margin: `0 auto`,
-          marginTop: rhythm(1.5),
-          marginBottom: rhythm(1.5),
-          maxWidth: 1500,
-          paddingLeft: rhythm(3 / 4),
-          paddingRight: rhythm(3 / 4),
-        }}
+            margin: `0 auto`,
+            marginTop: rhythm(1.5),
+            marginBottom: rhythm(1.5),
+            maxWidth: 1500,
+            paddingLeft: rhythm(3 / 4),
+            paddingRight: rhythm(3 / 4),
+          }}
         >
-        <Navbar/>
-        {this.props.children}
-        <Footer/>
-      </div>
+          {this.props.children}
+        </div>
       </>
     );
   }
