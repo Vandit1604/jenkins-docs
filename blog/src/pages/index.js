865ed3bae93ee3913e6830aa9cb67bd1f7e2b343
Kris Stern <krisstern@outlook.com>
2023-10-06 19:54:45 +0800

    feat: Align logo with text

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -140,12 +140,16 @@ class IndexPage extends React.Component {
           <div className={container}>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <MergeTypeIcon style={{ fontSize: "2rem", }} />
-                  <h4>
-                    Continuous Integration and Continuous Delivery
-                  </h4>
-                </div>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Continuous Integration and Continuous Delivery
+                    </h4>
+                  </Box>
+                </Box>
                 <p>
                   As an extensible automation server, Jenkins can be used as a simple
                   CI server or turned into the continuous delivery hub for any project.
@@ -154,27 +158,35 @@ class IndexPage extends React.Component {
             </div>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <FileDownloadOutlinedIcon style={{ fontSize: "2rem", }} />
-                  <h4>
-                    Easy installation
-                  </h4>
-                </div>
-                <p>
-                  Jenkins is a self-contained Java-based program, ready to run
-                  out-of-the-box, with packages for Windows, Linux, macOS and other
-                  Unix-like operating systems.
-                </p>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Easy installation
+                    </h4>
+                  </Box>
+                </Box>
+                  <p>
+                    Jenkins is a self-contained Java-based program, ready to run
+                    out-of-the-box, with packages for Windows, Linux, macOS and other
+                    Unix-like operating systems.
+                  </p>
               </div>
             </div>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <SettingsOutlinedIcon style={{ fontSize: "2rem", }} />
-                  <h4>
-                    Easy configuration
-                  </h4>
-                </div>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Easy configuration
+                    </h4>
+                  </Box>
+                </Box>
                 <p>
                   Jenkins can be easily set up and configured via its web interface,
                   which includes on-the-fly error checks and built-in help.
@@ -183,12 +195,16 @@ class IndexPage extends React.Component {
             </div>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <AppsOutlinedIcon style={{ fontSize: "2rem", }} />
-                  <h4>
-                    Plugins
-                  </h4>
-                </div>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Plugins
+                    </h4>
+                  </Box>
+                </Box>
                 <p>
                   With hundreds of plugins in the Update Center, Jenkins integrates
                   with practically every tool in the continuous integration and
@@ -198,13 +214,17 @@ class IndexPage extends React.Component {
             </div>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <ExtensionOutlinedIcon style={{ fontSize: "2rem", }} />
                   <ion-icon name="extension-puzzle-outline" role="img" className="md hydrated"></ion-icon>
-                  <h4>
-                    Extensible
-                  </h4>
-                </div>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Extensible
+                    </h4>
+                  </Box>
+                </Box>
                 <p>
                   Jenkins can be extended via its plugin architecture, providing
                   nearly infinite possibilities for what Jenkins can do.
@@ -213,13 +233,17 @@ class IndexPage extends React.Component {
             </div>
             <div className={box}>
               <div>
-                <div className={featureInfo}>
+                <Box className={featureInfo}
+                     sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }}
+                >
                   <SocialDistanceOutlinedIcon style={{ fontSize: "2rem", }} />
                   <ion-icon name="git-network-outline" role="img" className="md hydrated"></ion-icon>
-                  <h4>
-                    Distributed
-                  </h4>
-                </div>
+                  <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                    <h4>
+                      Distributed
+                    </h4>
+                  </Box>
+                </Box>
                 <p>
                   Jenkins can easily distribute work across multiple machines,
                   helping drive builds, tests and deployments across multiple
