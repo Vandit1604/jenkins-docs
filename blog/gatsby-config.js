c1def1b69dc699bc39c82601ce37a8f40a430154
Vandit Singh <vanditsinghkv@gmail.com>
2023-10-29 16:27:24 +0530

    fix the images not loading in blogposts

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: `src/images/jenkins.svg`, // This path is relative to the root of the site.
+        icon: `../blog/static/images/images/226px-Jenkins_logo.svg.png`, // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
