2825b8992eb700a15e4f9bce61f16e8b4a63ff97
Kris Stern <krisstern@outlook.com>
2023-11-19 12:33:54 +0800

    Tidy up

--- blog/src/pages/upgrade-guide.js
+++ blog/src/pages/upgrade-guide.js
@@ -9,8 +9,6 @@ const UpgradeGuide = () => (
     <PageName title={'Jenkins Upgrade Guide'} />
     <p>This section highlights important changes for administrators upgrading Jenkins LTS. Each section covers the upgrade from the previous LTS release, sections on versions x.y.1 cover the upgrade from the last release of the previous LTS line. if you are skipping LTS releases when upgrading, it is recommended to read the sections for all releases in between.
     </p>
-    <>
-    </>
   </IndexPageLayout>
 )
 
